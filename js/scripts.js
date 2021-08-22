dce94a25d317f589e6d2844ab910ebd477be90cb
Kevin Broja <kevin.broja@gus-group.com>
2021-08-22 18:30:16 +0200

    Imageslider v_1

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,9 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
-
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
