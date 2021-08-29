cd29b3b63d0506ac76352bec2f2840ea58d04870
Kevin Broja <kevin.broja@gus-group.com>
2021-08-29 14:54:17 +0200

    Datenschutz und back to top Button

--- js/scripts.js
+++ js/scripts.js
@@ -53,6 +53,31 @@ $("#submit").click(function() {
     }
 });
 //------------------VANILLAJS SCRIPT------------------------
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 window.addEventListener('DOMContentLoaded', event => {
     // Navbar shrink function
     var navbarShrink = function () {
